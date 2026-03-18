7f493dcd5e26b2e6a23a1856d20ca1eaf422f46b
Pawan Vishwakarma <pawanvishwakarmbharat@gmail.com>
2026-03-18 11:41:17 +0530

    Pacakage Name change

--- src/main/java/com/apigateway/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/api_gateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.apigateway;
+package com.apigateway.api_gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
